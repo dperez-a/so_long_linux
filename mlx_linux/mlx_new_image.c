8f105ad0a5ba899cf44f4e95bd3dc20adf3e31f2
Daniel Perez Ayuso <dperez-a@c1r17s1.42madrid.com>
2024-11-18 13:11:50 +0100

    changes on the maps

--- mlx_linux/mlx_new_image.c
+++ mlx_linux/mlx_new_image.c
@@ -21,12 +21,15 @@
 
 int	mlx_X_error;
 
-int	shm_att_pb(Display *d,XErrorEvent *ev)
+int shm_att_pb(Display *d, XErrorEvent *ev)
 {
-  if (ev->request_code==146 && ev->minor_code==X_ShmAttach)
-    write(2,WARN_SHM_ATTACH,strlen(WARN_SHM_ATTACH));
+  if (ev->request_code == 146 && ev->minor_code == X_ShmAttach) {
+    ssize_t bytes_written = write(2, WARN_SHM_ATTACH, strlen(WARN_SHM_ATTACH));
+    (void)bytes_written; // Ignorar el resultado de write.
+  }
   mlx_X_error = 1;
 }
+
 
 
 /*
